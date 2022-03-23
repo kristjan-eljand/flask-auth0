812d19d53c77cfab7f028cc384ed638609fe433f
Kristjan Eljand <kristjan.eljand@gmail.com>
2022-03-23 17:45:15 +0200

    requirements.txt and create_app function

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # full-flask-app
 # Environment setup
-## Test if you have python 3.9 or higher
-python3 -V
+**Test if you have python 3.9 or higher**
+* `python3 -V`
 
-### If you have older version, install python 3.9 (Linux 18.04 & 20.04)
+**If you have older version, install python 3.9 (Linux 18.04 & 20.04)**
 * `sudo apt-get update`
 * `sudo apt install software-properties-common`
 * `sudo add-apt-repository ppa:deadsnakes/ppa`
@@ -17,17 +17,25 @@ Type this command to set the new python version as a default one:
 * `sudo update-alternatives --config python3`
 The command line prompt will now ask you to select your version -> select the new installation version number.
 
-## Clone the code repo
-Clone the code repository and go to its main directory
+Check the python version again to see if the upgrade was successful
+* `python3 -V`
+
+**Install virtualenv package**
+* `sudo apt-get install virtualenv`
+
+**Clone the code repository and go to its main directory**
 * `git clone git@github.com:kristjan-eljand/full-flask-app.git`
 * `cd full-flask-app`
 
-Install virtualenv package
-* `sudo apt-get install virtualenv`
-
-Create virtualenv named venv and activate it
+**Create virtualenv named venv and activate it**
 * `virtualenv -p python3.9 venv`
 * `source venv/bin/activate`
 
-Install requirements file
+**Install requirements file**
 * `pip install -r requirements.txt`
+
+# Tutorial
+## General boilerplate elements
+1. To enable testing and production with separated configurations, we create an application through function (*application factory*) that lives in `__init__.py` file inside myapp folder [Flask web-page guidelines](https://flask.palletsprojects.com/en/2.0.x/tutorial/factory/).
+
+
---
+++ myapp/__init__.py
@@ -0,0 +1,51 @@
+import os
+
+from flask import Flask
+
+def create_app(test_config=None):
+    """Our application factory
+    
+    """
+    #=============================
+    # create and configure the app
+    #=============================
+    # __name__ tells the app where it is located
+    # instance_relative_config tells that configuration files are relative to instance folder
+    # the instance folder is located outside myapp folder and holds data like configuration secrets and database file
+    app = Flask(__name__, instance_relative_config=True)
+    
+    #=============================
+    # set default configuration
+    #=============================
+    # SECRET_KEY is dev to enable convenient development but should be overridden when deploying
+    # DATABASE is the path where the SQLite database file will be saved.
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'myapp.sqlite'),
+    )
+
+    #============================
+    # override default configuration
+    #============================
+    # app.config.from_pyfile takes values from config.py that is in instance folder
+    # and overrides the default configuration. This can be used to set the real SECRET_KEY
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # instance folder needs to be created if it doesn't exist
+    # to have a place to put database file
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, world!'
+
+    return app
